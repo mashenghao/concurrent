eb1da6f0964afc24db4cfc886b3347006c59434a
mahao2 <2727485259@qq.com>
2019-09-04 18:43:19 +0800

    Future设计模式

---
+++ src/main/java/dpart7/AsynFuture.java
@@ -0,0 +1,33 @@
+package dpart7;
+
+/**
+ * 具体的任务凭证,作为提交的异步的返回值（Future），
+ * 通过get获取到返回结果。
+ *
+ * @author: mahao
+ * @date: 2019/8/28
+ */
+public class AsynFuture<T> implements Future<T> {
+
+    //变量标识任务是否已经执行完毕
+    private volatile boolean done = false;
+
+
+    private T result;
+
+    //当异步执行线程执行完毕后，会调用方法，将结果传到这里
+
+    public synchronized void setResult(T result) {
+        this.result = result;
+        done = true;
+        notifyAll();//通知get方法，结果返回，唤醒执行
+    }
+
+    @Override
+    public synchronized T get() throws InterruptedException {
+        while (!done) {
+            wait();
+        }
+        return result;
+    }
+}
---
+++ src/main/java/dpart7/FutureTask.java
@@ -0,0 +1,17 @@
+package dpart7;
+
+/**
+ * 异步任务的接口，单独抽离出来，将执行与任务分开，解耦操作。
+ *
+ * @author: mahao
+ * @date: 2019/8/28
+ */
+public interface FutureTask<T> {
+
+    /**
+     * 执行的任务方法
+     *
+     * @return
+     */
+    T call();
+}
